64ac52cf02220fecea7346bed0432c62eff0f4af
francili0 <francilio.aguiar@ufu.br>
2025-04-15 18:23:28 -0300

    Alteraações

--- helloworld.py
+++
@@ -1,3 +0,0 @@
-print("Hello, world")
-print("Olá, Eu sou o Francílio Vieira Aguiar. Estou aprendendo a programar e já consigo rodar meus primeiros programas em Python.")
-print("Hello, world")
--- teste 1.py
+++
@@ -1,4 +0,0 @@
-input("Digite a idade: ")
-print ("A informação digitada foi:,input(Digite a idade: )")
-dado=input("Digite a idade: ")
-print("A informação digitada foi: , dado")
