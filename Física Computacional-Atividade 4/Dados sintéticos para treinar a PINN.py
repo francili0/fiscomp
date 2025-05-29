6e9b1debc08160e18a88428328ceda91f33fc176
francili0 <francilio.aguiar@ufu.br>
2025-05-29 12:02:34 -0300

    Alterações

--- Física Computacional-Atividade 4/Dados sintéticos para treinar a PINN.py
+++ Física Computacional-Atividade 4/Dados sintéticos para treinar a PINN.py
@@ -12,7 +12,7 @@ def T_analytical(t):
 
 # Gerar 10 pontos entre 0 e 200 s
 np.random.seed(42)  # reprodutibilidade
-t_data = np.linspace(0, 200, 10)
+t_data = np.linspace(0, 400, 10)
 T_clean = T_analytical(t_data)
 
 # Adicionar ruído gaussiano (média 0, desvio padrão 0.5)
@@ -21,7 +21,7 @@ T_noisy = T_clean + noise
 
 # Plot para visualização
 plt.figure(figsize=(8, 5))
-plt.plot(np.linspace(0, 200, 100), T_analytical(np.linspace(0, 200, 100)), 'k--', label='Solução analítica')
+plt.plot(np.linspace(0, 1000, 100), T_analytical(np.linspace(0, 1000, 100)), 'k--', label='Solução analítica')
 plt.scatter(t_data, T_noisy, color='red', label='Dados sintéticos com ruído', zorder=5)
 plt.xlabel('Tempo (s)')
 plt.ylabel('Temperatura (°C)')
